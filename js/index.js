27e0586e03aa38a2c4f6b85e9fbb3ae3c51ba23d
sasha-lane <natasha.lane1908@comcast.net>
2022-11-12 18:43:46 +0300

    HW_19 update

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$('document').ready(function () {
+    $('#viewSkills').on('click', () =>$('html, body').animate({
+        scrollTop: $("#skills").offset().top
+      }, 1000))
+})
+
+/* Link to video https://loom.com/share/3565780d338a491a8b65b2c776a62561 */
+
+/* Link to gif https://media.giphy.com/media/6ygQNXX5GhMfcoznj8/giphy.mp4 */
